9216b47757bb5a3ad8dca19174b71fb6bc439c30
John <madjohnabc@outlook.com>
2020-05-19 15:09:49 +0800

    cout

--- src/Uav_Tracking/src/formation.cpp
+++ src/Uav_Tracking/src/formation.cpp
@@ -148,11 +148,13 @@ void Formation::init()
 	//judge if all the agents in the network ready
 	int countAgent = 0;
 	int8_t tmpSynch = synch;
+	cout << "synch is:" << synch << endl;
 	while (tmpSynch != 0)
 	{
 		tmpSynch &= tmpSynch - 1;
 		countAgent++;
 	}
+	cout << "countAgent" << countAgent << endl;
 	if (countAgent < formationNum)
 	{
 		cout << "not enough ready agents" << endl;
